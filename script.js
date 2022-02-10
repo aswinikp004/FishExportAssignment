dd558b3e9664133f1ed2c014fe49f2f66f9baa1c
aswinikp004 <aswinikp004@gmail.com>
2022-02-10 15:21:30 +0100

    alert email

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+
+function detailsform()
+{
+	let email=document.getElementById("email");
+	let emailVal=email.value;
+
+	let passw=document.getElementById("pass");
+	let PassVal=passw.value;
+
+   	alert(" My email is  "+ emailVal + " " );
+}
+
+
